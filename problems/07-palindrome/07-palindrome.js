943bed3bf4dff5d6272c7acd9ebe98ddd32d3bd1
Lori Oliver <loridoesitbest@gmail.com>
2019-01-16 16:09:07 -0500

    Added palindrome problem

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,28 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
+    if(typeof someStr === 'string'){
+        let backwards = '';
+        let manipStr = someStr.replace('.', '').replace(' ','');
+        do{
+            manipStr = manipStr.replace('.', '').replace(' ','');
+        }while((manipStr.includes(' ')) || (manipStr.includes('.')));
+        manipStr = manipStr.toLowerCase();
+        
+        for (let i = manipStr.length -1; i >= 0; i--) {
+            backwards += manipStr.charAt(i);          
+        }
+        if(manipStr === backwards){
+            return true;
+        }else{
+            //DEBUG console.log(`The backwards version is ${backwards}`);
+            return false;
+        }
+    }else{
+        return "Please enter a string value!";
+    }
   
 }
+console.log(isPalindrome("nurses run"));
+console.log(isPalindrome(1));
+console.log(isPalindrome('A nut for a jar of tuna.'));
