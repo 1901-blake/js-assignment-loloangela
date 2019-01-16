de40cb74cacaec0b411a1acfdc6d3b3b2f3017d5
Lori Oliver <loridoesitbest@gmail.com>
2019-01-16 12:26:37 -0500

    Added factorial problem

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,11 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    if(sumNum === 1){
+        return sumNum;
+    }else{
+       return sumNum * factorial(sumNum - 1);
+    }
 }
+let x = 5;
+console.log(`Factorial of ${x} is: ${factorial(x)}`);
