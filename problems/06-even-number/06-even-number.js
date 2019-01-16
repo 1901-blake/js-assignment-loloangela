95d21b30a91bba2b12aa2d65294e4ea49427be7d
Lori Oliver <loridoesitbest@gmail.com>
2019-01-16 14:57:03 -0500

    Add even-number problem

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,17 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
+
 function isEven(someNum) {
-  
+  if((someNum > 0) && (someNum < 1)){
+      return false;
+  }else if(someNum === 1){
+      return true;
+  }else{
+      //DEBUG console.log(`Division is ${someNum/2}`);
+      return isEven(someNum/2);
+  }
 }
+console.log(isEven(5));
+console.log(isEven(2));
+console.log(isEven(25));
