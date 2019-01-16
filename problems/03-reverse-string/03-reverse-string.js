216c6337ebf3f22104ff14fbd7fb8978a6c89b8f
Lori Oliver <loridoesitbest@gmail.com>
2019-01-15 19:50:37 -0500

    Reverse String problem finished

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,14 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  let str = '';
+  let arrStr = someStr.split('');
+
+  for (let index = arrStr.length -1 ; index >= 0; index--) {
+      str += arrStr[index];
+  }
+  return str;
 }
+
+console.log(reverseStr("Stranger Things is a movie about strange occurrances!"));
+console.log(reverseStr("What is do?"));
