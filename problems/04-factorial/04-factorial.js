cf207469e2035cee0a1040fa661b4bb51f709a99
Lori Oliver <loridoesitbest@gmail.com>
2019-01-19 11:53:34 -0500

    Used ternary operator in factorial problem

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,11 +2,7 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-    if(sumNum === 1){
-        return sumNum;
-    }else{
-       return sumNum * factorial(sumNum - 1);
-    }
+    return (sumNum === 1) ? sumNum : (sumNum * factorial(sumNum -1));
 }
 let x = 5;
 console.log(`Factorial of ${x} is: ${factorial(x)}`);
