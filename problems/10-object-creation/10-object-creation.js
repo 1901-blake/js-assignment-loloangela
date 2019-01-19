2090dbb1b7ababb6fc0686043e4f2d9d12aab760
Lori Oliver <loridoesitbest@gmail.com>
2019-01-18 23:54:21 -0500

    update problem 10

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,54 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+// Object Literals of each Person
+let person = {
+    name: 'Lori',
+    age: 31
+}
+
+let person1 = {
+    name: 'Lauren',
+    age: 27
+}
+
+let person2 = {
+    name: 'Lisa',
+    age: 22
+}
+
+// Class (created through the use of the function keyword) accepts a person object and additional features 
+function addFieldsToPerson(newPerson, ht, hairColor){
+    this.whoIAm = newPerson,
+    this.height = ht,
+    this.hairColor = hairColor
+
+    this.discussMyself = () => {
+        console.log(`\tMy Name is ${this.whoIAm['name']} and I am ${this.whoIAm['age']} years old.
+        I'm ${this.height}ft tall. My hair color is ${this.hairColor}.\n`);
+    }
+}
+
+// The class People wraps several person's and and calls their discussMyself method
+class People {
+    constructor(onePerson, anotherPerson, extraPerson) {
+        this.onePerson = onePerson,
+        this.anotherPerson = anotherPerson,
+        this.extraPerson = extraPerson
+    }
+
+    introduce() {
+        let p0 = new addFieldsToPerson(this.onePerson, 5, 'black');
+        let p1 = new addFieldsToPerson(this.anotherPerson, 5.11, 'blonde');
+        let p2 = new addFieldsToPerson(this.extraPerson, 5.5, 'burgandy');
+
+        p0.discussMyself();
+        p1.discussMyself();
+        p2.discussMyself();
+    }
+}
+
+//DEBUG console.log(person);
+let group = new People(person, person1, person2);
+group.introduce();
