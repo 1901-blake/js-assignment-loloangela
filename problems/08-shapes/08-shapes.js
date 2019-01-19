b1c687ecdb1449d6e2e4deb50d52bc65b0d061b2
Lori Oliver <loridoesitbest@gmail.com>
2019-01-18 19:58:12 -0500

    Added Shapes and Object Properties

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,62 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  switch (shape.toLowerCase()) {
+    case 'square':
+      for (let i = 0; i < height; i++) {
+        let row = '';
+        for (let j = 0; j < height; j++) {
+          row += character;  
+        }
+        console.log(row);
+      }
+      break;
+    case 'triangle':
+      for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 0; j < i; j++) {
+          row += character;
+        }
+        console.log(row);
+      }
+      break;
+    case 'diamond':
+      // Create an array the size of height
+      let space = ' ';
+      let startPos = Math.floor(height/2);
+      // Populate array with character in appropriate postion
+      let i = 0;
+      for (i; i <= Math.floor(height/2); i++) {
+        row = '';
+        let numChar = i * 2 + 1;
+        //DEBUG console.log(`NumChar: ${numChar}`);
+        //DEBUG console.log(`StartPos: ${startPos}`);
+        let numSpaces = startPos;
+        //DEBUG console.log(`NumSpaces: ${numSpaces}`);
+        row += space.repeat(numSpaces);
+        row += character.repeat(numChar);
+        row += space.repeat(numSpaces);
+        console.log(row);
+        startPos = startPos/2;
+      }
+      //DEBUG console.log(`Index: ${i}`);
+      for (--i; i > 0; i--) {
+        row = '';
+        //DEBUG console.log(`Index inside loop: ${i}`);
+        startPos += 1;
+        numChar = i * 2 -1;
+        let numSpaces = startPos;
+        row += space.repeat(numSpaces);
+        row += character.repeat(numChar);
+        row += space.repeat(numSpaces);
+        console.log(row);
+      }
+      
+
+      break;
+  }
 }
+
+printShape('square', 5, '*');
+printShape('triangle', 7, '$');
+printShape('diamond', 5, '^');
--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,13 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    console.log(someObj);
 }
+
+let cat = {
+    meow: 'Meow',
+    age : [2, 16, 75, 32],
+    health: 65
+};
+    
+objectProperties(cat);
