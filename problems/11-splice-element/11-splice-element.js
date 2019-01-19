78b53fa7120b91e14ebede4846a1da1301228533
Lori Oliver <loridoesitbest@gmail.com>
2019-01-19 10:06:15 -0500

    Removed extras from problem 11

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -4,22 +4,10 @@ Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
   // If by splicing we want to remove a specific element
-  //someArr.splice(index, 1);
-/*
-  // If by splicing we want to break the array in half 
-  // Removing the specified index and elements after it
-  let amntToRemove = someArr.length - index;
-  console.log(`Amount to remove ${amntToRemove}`);
-  someArr.splice(index, amntToRemove);
-*/
-  // If by splicing we want to break the array in half 
-  // Removing after the specified index and elements after it
-  let amntToRemove = someArr.length - ++index;
-  console.log(`Amount to remove ${amntToRemove}`);
-  someArr.splice(index, amntToRemove);
-
+  someArr.splice(index, 1);
   return someArr;
 }
+
 let myArr = ['Good', 'Morning', 'Miami', 1, 2, 3, {
     name: 'apple',
     classification: 'fruit'
